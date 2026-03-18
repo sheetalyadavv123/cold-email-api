dd51100be501a1ea5121ff7dfe261099cd287ba9
sheetalyadavv123 <sheetalyad7@gmail.com>
2026-03-18 20:01:33 +0530

    fix: add missing express require

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const morgan = require("morgan");
-
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
-
-const rateLimit = require("express-rate-limit");
+const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
-const app = require("express");
-const PORT = 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
